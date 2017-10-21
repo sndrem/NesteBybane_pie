1e5b192459a8228f709cf86bfa2ec554ac7e0592
Sindre <sndrem@live.no>
2017-10-21 11:25:04 +0200

    Now it is the correct url for flesland

--- nesteBybane.py
+++ nesteBybane.py
@@ -31,7 +31,7 @@ day = now.day
 hour = now.hour
 minute = now.minute
 SKYSS_URL_BYPARK = "https://reiseplanlegger.skyss.no/scripts/TravelMagic/TravelMagicWE.dll/svar?from=Brann%20stadion,%20bybanestopp%20%28Bergen%29&to=Byparken,%20bybanestopp%20%28Bergen%29&direction=1&lang=nn&instant=1&date=" + str(day) + "." + str(month) + "." + str(year) + "&time=" + str(hour) + ":" + str(minute)
-SKYSS_URL_FLESLAND = "https://www.skyss.no/Rutetider-og-kart1/Reiseplanleggar/?from=Brann+stadion%2C+bybanestopp+%28Bergen%29&to=Flesland+%28Bergen%29&direction=1&date=" + str(day) + "." + str(month) + "." + str(year) + "&time=" + str(hour) + ":" + str(minute)
+SKYSS_URL_FLESLAND = "https://reiseplanlegger.skyss.no/scripts/TravelMagic/TravelMagicWE.dll/svar?from=Brann stadion, bybanestopp %28Bergen%29&to=Flesland %28Bergen%29&direction=1&lang=nn&instant=1&date=" + str(day) + "." + str(month) + "." + str(year) + "&time=" + str(hour) + ":" + str(minute)
 SKYSS_URLS = [SKYSS_URL_BYPARK, SKYSS_URL_FLESLAND, SKYSS_URL_BYPARK]
 
 class BybaneTime():
